678dff5d4ebb232f2d557731d6ae36d8bf15d7e5
zty200489 <zty200489@gmail.com>
2022-03-18 22:06:46 +0800

    first commit

---
+++ generate-proverb.js
@@ -0,0 +1,11 @@
+﻿const paragraph = document.querySelector('p.proverb');
+
+list = new Array(
+    '我在客观上是幸福的，谁也无可非议。那么，我也应该有权蔑视幸福。——三岛由纪夫',
+    '夜神不可能总是伴随我们，但我们能伪造黑夜。——埃德加·爱伦坡',
+    '人活在必然性中，这并不是一个必然性。——伊壁鸠鲁'
+);
+
+let index = Math.floor(Math.random() * list.length);
+
+paragraph.textContent = list[index];
